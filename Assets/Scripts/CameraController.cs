c370d3d2ac91b401d8b121f5d2d9873153b4313e
TomTaco <43515248+TomTaco@users.noreply.github.com>
2021-04-17 02:05:04 +0200

    Crazy Überraschung

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -5,16 +5,27 @@ using UnityEngine;
 public class CameraController : MonoBehaviour
 {
     public GameObject player;
+    public float timeValue;
+    public Camera cam1;
+    public Camera cam2;
 
     private Vector3 offset;
     void Start()
     {
         offset = transform.position - player.transform.position;
+        timeValue = 9;
+        cam1.enabled = true;
+        cam2.enabled = false;
     }
 
     // Update is called once per frame
     void LateUpdate()
     {
         transform.position = player.transform.position + offset;
+        timeValue -= Time.deltaTime;
+        if(timeValue < 0 ){
+            cam2.enabled = true;
+            cam1.enabled = false;
+        }
     }
 }
