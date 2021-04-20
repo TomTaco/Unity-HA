bf2acdaeda914c31a9ee6e69ab2f30326619012a
KoopaKingDaTrooperPing <82676741+KoopaKingDaTrooperPing@users.noreply.github.com>
2021-04-20 21:17:57 +0200

    Revert "öhm"
    This reverts commit 44cc5f5401c2f121a3d5f2504127926dcd0ec5c5.

--- Assets/Scripts/DoorOpener.cs
+++ Assets/Scripts/DoorOpener.cs
@@ -1,20 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class DoorOpener : MonoBehaviour
 {
+
+    private Animator anim;
+    public GameObject door;
     private GameObject sphere;
     private bool isPressed;
     public AudioClip buttonClickClip;
-    public GameEvent ButtonPressEvent;
+    void Start()
+    {
+        anim = door.gameObject.GetComponent<Animator>();
+        sphere = gameObject.transform.GetChild(1).gameObject;
+        isPressed = false;
+        anim.SetBool("buttonPressed",false);
+    }
 
     void OnTriggerEnter(Collider other){
         if(other.gameObject.CompareTag("Player")){
             if(!isPressed){
-                ButtonPressEvent.Raise();
                 AudioSource.PlayClipAtPoint(buttonClickClip, other.transform.position);
-                sphere = gameObject.transform.GetChild(1).gameObject; //Sphere
                 sphere.transform.Translate(Vector3.down*0.06f);
                 sphere.GetComponent<Renderer>().material.SetColor("_Color",Color.green);
+                anim.SetBool("buttonPressed",true);
                 isPressed = true;
             }
         }
